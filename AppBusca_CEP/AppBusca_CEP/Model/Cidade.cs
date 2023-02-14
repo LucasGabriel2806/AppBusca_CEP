ab906aafea53fdb8794ab15fb3ae450805ff3c39
Usuario <Usuario@CPD03-M11>
2023-02-14 15:33:41 -0300

    Adição de Model e Service.

---
+++ AppBusca_CEP/AppBusca_CEP/Model/Cidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBusca_CEP.Model
+{
+    public class Cidade
+    {
+        public int id_cidade { get; set; }
+        public string descricao { get; set; }
+        public string uf { get; set; }
+        public int codigo_ibge { get; set; }
+        public int ddd { get; set; }
+
+
+
+
+    }
+}
---
+++ AppBusca_CEP/AppBusca_CEP/Service/DataService.cs
@@ -0,0 +1,62 @@
+﻿using AppBusca_CEP.Model;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.ConstrainedExecution;
+
+
+namespace AppBusca_CEP.Service
+{
+    public class DataService
+    {
+        public static async Task<Endereco> GetEnderecoByCep(string cep)
+        {
+            Endereco end;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/endereco/by-cep?cep=" + cep);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    end = JsonConvert.DeserializeObject<Endereco>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+
+            return end;
+        }
+
+        public static async Task<List<Bairro>> GetBairrosByIdCidade(int id_cidade)
+        {
+            List<Bairro> arr_bairros = new List<Bairro>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/bairro/by-cidade?id_cidade=" + id_cidade);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    arr_bairros = JsonConvert.DeserializeObject<List<Bairro>>(json);
+
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+
+            return arr_bairros;
+
+
+        }
+
+
+    }
+}
